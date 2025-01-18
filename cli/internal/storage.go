e3b80cbc06523625f93eca3a6852028fead96054
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-01-18 14:05:24 +0000

    implementing the LoadTask Function.

--- cli/internal/storage.go
+++ cli/internal/storage.go
@@ -22,3 +22,25 @@ func SaveTasks(tasks []task.Task) error {
     _, err = file.Write(data)
     return err
 }
+
+
+// LoadTasks will load tasks from a JSON file into the provided slice.
+func LoadTasks(tasks *[]task.Task) error {
+	file, err := os.Open("tasks.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	defer file.Close()
+
+	data, err := os.ReadFile("tasks.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	return json.Unmarshal(data, tasks)
+} 
