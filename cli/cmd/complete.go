2d397549a75a4e139270cd369d9ab9c2a219a21e
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-01-18 12:59:25 +0000

    working on adding complete functionality to tasks

---
+++ cli/cmd/complete.go
@@ -0,0 +1,12 @@
+package cmd
+
+import (
+ 
+    "github.com/spf13/cobra"
+  )
+
+var completeCmd = &cobra.Command{
+    Use:   "complete [task ID]",
+    Short: "Mark a task as completed",
+}
+	func main () {}
