5d02c8584a3b161aebe294fbb0b68aa274478dc6
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-01-18 13:04:05 +0000

    Add 'complete' command to mark tasks as completed with error handling

--- cli/cmd/complete.go
+++ cli/cmd/complete.go
@@ -1,12 +1,35 @@
 package cmd
 
 import (
- 
-    "github.com/spf13/cobra"
-  )
+    "fmt"
+    "os"
+    "strconv"
 
+    "github.com/spf13/cobra"
+	"github.com/mohammedfirdouss/go-task-manager/cli/internal/task"
+)
+ 
 var completeCmd = &cobra.Command{
     Use:   "complete [task ID]",
     Short: "Mark a task as completed",
+    Args:  cobra.ExactArgs(1),
+    Run: func(cmd *cobra.Command, args []string) {
+        id, err := strconv.Atoi(args[0])
+        if err != nil {
+            fmt.Printf("Invalid task ID: %v\n", err)
+            os.Exit(1)
+        }
+
+        err = task.CompleteTask(id)
+        if err != nil {
+            fmt.Printf("Error completing task: %v\n", err)
+            os.Exit(1)
+        }
+
+        fmt.Println("Task completed successfully!")
+    },
 }
-	func main () {}
+
+func init() {
+    rootCmd.AddCommand(completeCmd)
+}
