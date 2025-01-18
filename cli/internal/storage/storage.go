8096675e24b6eadaa6ba9fea14ad1e39357c2188
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-01-18 14:24:28 +0000

    implement SaveTasks and LoadTasks functions for task persistence

---
+++ cli/internal/storage/storage.go
@@ -0,0 +1,44 @@
+package storage
+
+import (
+    "encoding/json"
+    "os"
+    "github.com/mohammedfirdouss/go-task-manager/cli/internal/task"
+)
+
+type Storage struct{}
+
+// SaveTasks saves the current list of tasks to a JSON file.
+func (s *Storage) SaveTasks(tasks []task.Task) error {
+    file, err := os.Create("tasks.json")
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    data, err := json.Marshal(tasks)
+    if err != nil {
+        return err
+    }
+
+    _, err = file.Write(data)
+    return err
+}
+
+// LoadTasks loads tasks from a JSON file into the provided slice.
+func (s *Storage) LoadTasks(tasks *[]task.Task) error {
+    file, err := os.Open("tasks.json")
+    if err != nil {
+        if os.IsNotExist(err) {
+            return nil
+        }
+        return err
+    }
+    defer file.Close()
+
+    data, err := os.ReadFile("tasks.json")
+    if err != nil {
+        return err
+    }
+    return json.Unmarshal(data, tasks)
+}
