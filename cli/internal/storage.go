e678d203e2d8e95c8ea3b90da737f83062ec95e6
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-01-18 14:04:05 +0000

    implementing the SaveTasks Function

--- cli/internal/storage.go
+++ cli/internal/storage.go
@@ -1,8 +1,24 @@
 package internal
 
 import (
+    "encoding/json"
     "os"
     "fmt"
 )
 
-func main () {}
+// SaveTasks will save the current list of tasks to a JSON file.
+func SaveTasks(tasks []task.Task) error {
+    file, err := os.Create("tasks.json")
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    data, err := json.Marshal(tasks)
+    if err != nil {
+        return err
+    }
+
+    _, err = file.Write(data)
+    return err
+}
