eef8c303830a905d726ba95931f9fa43ca86aeba
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-01-21 13:40:45 +0000

    Initialize task storage before listing tasks in list command

--- cli/cmd/list.go
+++ cli/cmd/list.go
@@ -2,16 +2,25 @@ package cmd
 
 import (
     "fmt"
-
     "github.com/spf13/cobra"
     "github.com/mohammedfirdouss/go-task-manager/cli/internal/task"
-   
+    "github.com/mohammedfirdouss/go-task-manager/cli/internal/storage"
 )
 
 var listCmd = &cobra.Command{
     Use:   "list",
     Short: "List all tasks",
     Run: func(cmd *cobra.Command, args []string) {
+        // Create an instance of Storage (which implements TaskStorage)
+        storage := &storage.Storage{}
+
+        // Initialize the task list using the storage instance
+        err := task.Init(storage)
+        if err != nil {
+            fmt.Printf("Error initializing tasks: %v\n", err)
+            return
+        }
+
         tasks := task.ListTasks()
         for _, t := range tasks {
             status := "Incomplete"
