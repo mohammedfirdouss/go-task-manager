73335e1b284ad480fe642fb172791ab6b55e4066
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-01-21 13:53:33 +0000

    Initialize tasks before completing to ensure task list is loaded

--- cli/cmd/complete.go
+++ cli/cmd/complete.go
@@ -20,20 +20,27 @@ var completeCmd = &cobra.Command{
             fmt.Printf("Invalid task ID: %v\n", err)
             os.Exit(1)
         }
-
+    
         // Create an instance of Storage (which implements TaskStorage)
         storage := &storage.Storage{}
-
+    
+        // Initialize tasks from storage
+        err = task.Init(storage)
+        if err != nil {
+            fmt.Printf("Error initializing tasks: %v\n", err)
+            os.Exit(1)
+        }
+    
         // Pass the storage instance to CompleteTask
         err = task.CompleteTask(id, storage)
         if err != nil {
             fmt.Printf("Error completing task: %v\n", err)
             os.Exit(1)
         }
-
+    
         fmt.Println("Task completed successfully!")
     },
-}
+}  
 
 func init() {
     rootCmd.AddCommand(completeCmd)
