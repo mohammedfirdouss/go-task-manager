ced1685fc27e56215a82b7476122d22532cc058c
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-01-23 00:06:00 +0000

    forgot to update this sincee

--- cli/main.go
+++ cli/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+    "fmt"
+    "os"
+
     "github.com/mohammedfirdouss/go-task-manager/cli/cmd"
+    "github.com/mohammedfirdouss/go-task-manager/cli/internal/storage"
+    "github.com/mohammedfirdouss/go-task-manager/cli/internal/task"
 )
 
 func main() {
+    storage := &storage.Storage{}
+    err := task.Init(storage)
+    if err != nil {
+        fmt.Printf("Error initializing tasks: %v\n", err)
+        os.Exit(1)
+    }
     cmd.Execute()
 }
